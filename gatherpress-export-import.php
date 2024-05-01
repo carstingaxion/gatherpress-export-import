d5fec7f9e8a8c31df3632a5046fc2aa1714adba3
Carsten Bach <mail@carsten-bach.de>
2024-05-01 23:04:40 +0200

    Create gatherpress-export-import.php

---
+++ gatherpress-export-import.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: GatherPress | Export & Import
+Plugin URI:
+Description: Use WordPress' native export & import tools for GatherPress data
+Version: 0.1.0
+Author: Carsten Bach
+Author URI: https://gatherpress.org
+Text Domain: gatherpress-export-import
+Domain Path: /languages
+Requires Plugins: wordpress-importer
+*/
